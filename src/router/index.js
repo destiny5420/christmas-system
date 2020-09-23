be795c5c23046bb1ed6ebd5f8bbb8a11e17f46f2
paper.window <destiny5420@gmail.com>
2020-09-23 08:25:13 +0800

    [Add] init

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,24 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Login from '@/views/vLogin/index.vue';
+import Dashboard from '@/views/vDashboard/index.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home,
+    path: '*',
+    redirect: '/login',
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/login',
+    name: 'vLogin',
+    component: Login,
+  },
+  {
+    path: '/dashboard',
+    name: 'vDashboard',
+    component: Dashboard,
   },
 ];
 
